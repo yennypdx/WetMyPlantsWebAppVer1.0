efbbf718e27e32cccba1b04f8c138858d322f2d4
Thea VanKessel <thea.vankessel@oit.edu>
2019-01-14 10:34:09 -0800

    Registration, Login, ForgotPassword View and ViewModels

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
