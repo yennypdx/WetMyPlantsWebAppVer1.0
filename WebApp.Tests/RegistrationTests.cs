9cb12a9577ef1cfc4800344c0426edd33040acfb
ypurwantopdx <yenny.purwanto@oit.edu>
2019-01-13 20:52:03 -0800

    Added registration test file.

---
+++ WebApp.Tests/RegistrationTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApp.Tests
+{
+    [TestClass]
+    public class RegistrationTests
+    {
+        [TestMethod]
+        public void UserNameTest_ShouldPassWhenDbReturnTheSampleUserFirstName()
+        {
+            // TODO: Add test logic here
+            
+        }
+
+        [TestMethod]
+        public void UserNameTest_ShouldFailWhenDbReturnNonSampleUserFirstName()
+        {
+            // TODO: Add test logic here
+            
+        }
+
+        [TestMethod]
+        public void RegUserNameTest_ShouldPassWhenDbReturnTheSampleUserLastName()
+        {
+            // TODO: Add test logic here
+
+        }
+
+        [TestMethod]
+        public void RegUserNameTest_ShouldFailWhenDbReturnNonSampleUserLastName()
+        {
+            // TODO: Add test logic here
+
+        }
+
+        [TestMethod]
+        public void RegEmailTest_ShouldPassWhenDbReturnTheSampleEmail()
+        {
+            // TODO: Add test logic here
+
+        }
+
+        [TestMethod]
+        public void RegEmailTest_ShouldFailWhenDbReturnNonSampleEmail()
+        {
+            // TODO: Add test logic here
+
+        }
+
+        [TestMethod]
+        public void RegPhoneTest_ShouldPassWhenDbReturnSampleNumber()
+        {
+            // TODO: Add test logic here
+
+        }
+
+        [TestMethod]
+        public void RegPhoneTest_ShouldFailWhenDbReturnNonSampleNumber()
+        {
+            // TODO: Add test logic here
+
+        }
+    }
+}
