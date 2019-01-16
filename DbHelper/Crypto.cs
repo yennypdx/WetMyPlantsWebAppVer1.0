5a3b774c24909ae6fcd8b1129818aa9726358669
Andy <andyjhorn@gmail.com>
2019-01-15 22:24:40 -0800

    Implemented and tested a Crypto class to provide password hashing and authentication.

---
+++ DbHelper/Crypto.cs
@@ -0,0 +1,31 @@
+﻿// Andrew Horn
+// 0/15/2019
+//
+// Crypto class to provide secure cryptography
+// and authentication by hashing and verifying passwords.
+// Utilizes the BCrypt.Net package to provide the methods.
+// Information found on https://cmatskas.com/a-simple-net-password-hashing-implementation-using-bcrypt/
+
+
+namespace DbHelper
+{
+    public static class Crypto
+    {
+        public static string GetNewSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(10);
+        }
+
+        public static string HashPassword(string password, string salt = null)
+        {
+            return salt != null
+                ? BCrypt.Net.BCrypt.HashPassword(password, salt)
+                : BCrypt.Net.BCrypt.HashPassword(password, 10);
+        }
+
+        public static bool ValidatePassword(string plainTextPassword, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainTextPassword, hashedPassword);
+        }
+    }
+}
---
+++ DbHelperTests/CryptoTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using DbHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DbHelperTests
+{
+    [TestClass]
+    public class CryptoTests
+    {
+        [TestMethod]
+        public void CryptoTestGetNewSalt()
+        {
+            var salt = Crypto.GetNewSalt();
+
+            Assert.IsNotNull(salt);
+        }
+
+        [TestMethod]
+        public void CryptoTestGeneratePasswordHashAndSalt()
+        {
+            var salt = Crypto.GetNewSalt();
+            var hash = Crypto.HashPassword("password", salt);
+
+            Assert.IsNotNull(hash);
+        }
+
+        [TestMethod]
+        public void CryptoTestGeneratePasswordHashOnly()
+        {
+            var hash = Crypto.HashPassword("password");
+
+            Assert.IsNotNull(hash);
+        }
+
+        [TestMethod]
+        public void CryptoTestVerifyHashedPasswordWithSalt()
+        {
+            var password = "password";
+            var salt = Crypto.GetNewSalt();
+            var hash = Crypto.HashPassword(password, salt);
+
+            var isCorrect = Crypto.ValidatePassword(password, hash);
+            Assert.IsTrue(isCorrect);
+        }
+
+        [TestMethod]
+        public void CryptoTestVerifyHashPasswordNoSalt()
+        {
+            var password = "password";
+            var hash = Crypto.HashPassword(password);
+
+            var isCorrect = Crypto.ValidatePassword(password, hash);
+            Assert.IsTrue(isCorrect);
+        }
+
+        [TestMethod]
+        public void CryptoTestVerifyBadPassword()
+        {
+            var correctPassword = "password";
+            var incorrectPassword = "notPassword";
+
+            var hash = Crypto.HashPassword(correctPassword);
+
+            var isCorrect = Crypto.ValidatePassword(incorrectPassword, hash);
+            Assert.IsFalse(isCorrect);
+        }
+    }
+}
