ac91c47dfc83e454c82f0c2539282ef0f8468e20
Carter Knutson <carter.knutson@oit.edu>
2019-01-15 20:59:14 -0800

    comment

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//merge damn it
 namespace WebApp.Controllers
 {
     public class AccountController : Controller
