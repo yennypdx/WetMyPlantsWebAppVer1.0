5cc7653bf55a0a71b1a92cdc61fe23cd9d3f7b17
Andy <andyjhorn@gmail.com>
2019-01-16 09:09:32 -0800

    Crypto fully tested.

---
+++ DbHelper.Test/CryptoTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DbHelper.Test
+{
+    [TestClass]
+    public class CryptoTest
+    {
+        [TestMethod]
+        public void CryptoGetHashedPasswordTest()
+        {
+            var pwd = "password";
+
+            var hash = Crypto.HashPassword(pwd);
+
+            Assert.AreNotEqual(pwd, hash);
+        }
+
+        [TestMethod]
+        public void CryptoValidateCorrectPasswordTest()
+        {
+            var pwd = "password";
+            var hash = Crypto.HashPassword(pwd);
+
+            var isCorrect = Crypto.ValidatePassword(pwd, hash);
+            Assert.IsTrue(isCorrect);
+        }
+
+        [TestMethod]
+        public void CryptoValidateIncorrectPasswordFailTest()
+        {
+            var pwd = "password";
+            var wrongPwd = "wrongPassword";
+
+            var hash = Crypto.HashPassword(pwd);
+
+            var isCorrect = Crypto.ValidatePassword(wrongPwd, hash);
+
+            Assert.IsFalse(isCorrect);
+        }
+    }
+}
