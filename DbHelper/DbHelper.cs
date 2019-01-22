fe0a41fba0fbdae39355a76665ff96b6585546eb
Andy <andyjhorn@gmail.com>
2019-01-21 20:41:27 -0800

    Implemented DbHelper helper methods and CreateNewUser. Updated AccountController to use the DbHelper and CreateNewUser method. Removed UserModel from WebApp and implemented User model class in new Models project.

--- DbHelper/DbHelper.cs
+++ DbHelper/DbHelper.cs
@@ -1,7 +1,117 @@
-﻿namespace DBHelper
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using DbHelper;
+using Models;
+
+namespace DBHelper
 {
+    enum UserColumns
+    {
+        Id,
+        FirstName,
+        LastName,
+        Phone,
+        Email,
+        Hash
+    }
+
     public class DbHelper
     {
-        private readonly string dbConnectionString = AccessHelper.GetDbConnectionString();
+        private readonly string _dbConnectionString = AccessHelper.GetDbConnectionString();
+        private readonly SqlConnection _dbConnection;
+
+        public DbHelper()
+        {
+            _dbConnection = new SqlConnection(_dbConnectionString);
+        }
+
+        // Open() makes it easier to use the SqlConnection without worrying about
+        // its current state. It will always be open when you need it.
+        private SqlConnection Open()
+        {
+            _dbConnection.ConnectionString = _dbConnectionString;
+            if (_dbConnection.State != ConnectionState.Open)
+                _dbConnection.Open();
+            return _dbConnection;
+        }
+
+        private string RunScalar(string commandString)
+        {
+            var sqlCommand = new SqlCommand(commandString, _dbConnection);
+
+            using(Open())
+            {
+                var result = sqlCommand.ExecuteScalar()?.ToString();
+                return result;
+            }
+        }
+
+        private DataTableReader RunReader(string connectionString)
+        {
+            var sqlCommand = new SqlCommand(connectionString, _dbConnection);
+            var adapter = new SqlDataAdapter(sqlCommand);
+            var dataSet = new DataSet();
+
+            using(Open())
+            {
+                adapter.Fill(dataSet);
+                return dataSet.CreateDataReader();
+            }
+        }
+
+        private bool RunNonQuery(string connectionString)
+        {
+            var sqlCommand = new SqlCommand(connectionString, _dbConnection);
+
+            try
+            {
+                using(Open())
+                {
+                    var result = sqlCommand.ExecuteNonQuery();
+                    return result != 0;
+                }
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        private User FindUserByEmail(string email)
+        {
+            if (email == null) return null;
+
+            var queryString = $"SELECT * FROM Users WHERE Email = '{email}';";
+
+            var result = RunReader(queryString);
+
+            if (!result.HasRows) return null;
+
+            result.Read();
+            var user = new User
+            {
+                Id = result.GetString((int) UserColumns.Id),
+                FirstName = result.GetString((int) UserColumns.FirstName),
+                LastName = result.GetString((int) UserColumns.LastName),
+                Email = result.GetString((int) UserColumns.Email),
+                Phone = result.GetString((int) UserColumns.Phone),
+                Hash = result.GetString((int) UserColumns.Hash)
+            };
+            return user;
+        }
+
+        public bool CreateNewUser(string firstName, string lastName, string phone, string email, string password)
+        {
+            if (FindUserByEmail(email) != null) return false;
+
+            var passwordHash = Crypto.HashPassword(password);
+
+            var queryString = "INSERT INTO Users (FirstName, LastName, Phone, Email, Hash) " +
+                              $"VALUES ({firstName}, {lastName}, {phone}, {email}, {passwordHash})";
+
+            var result = RunNonQuery(queryString);
+            return result;
+        }
     }
 }
--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DBHelper;
 using WebApp.Models;
 using WebApp.Models.AccountViewModels;
 
@@ -11,6 +12,12 @@ namespace WebApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly DBHelper.DbHelper _db;
+
+        public AccountController()
+        {
+            _db = new DBHelper.DbHelper();
+        }
         // GET: Account
         public ActionResult Index()
         {
@@ -46,7 +53,7 @@ namespace WebApp.Controllers
                     var email = uModel.Email;
                     var pass = uModel.Password;
 
-                    SubmitUserDataToDb(fname, lname, phn, email, pass);
+                    _db.CreateNewUser(fname, lname, phn, email, pass);
                     return View("Register");
                 }
                 else
@@ -60,33 +67,6 @@ namespace WebApp.Controllers
             }
         }
 
-        public void SubmitUserDataToDb(string inFname, string inLname, string inPhn, string inEmail, string inPass)
-        {
-            //TODO: get the connection from DBHelper
-            var connectionStr = "";
-            //TODO: reconfirm the sequence of the attributes in the table
-            var queryInsertDataToUserDb = "INSERT INTO Users (FirstName, LastName, Password, Email, Password) " +
-                                          "VALUES (@fname, @lname, @phn, @mail, @pass)";
-
-            using (var connection = new SqlConnection(connectionStr))
-            {
-                var commandUserDb = new SqlCommand(queryInsertDataToUserDb, connection);
-                commandUserDb.Parameters.AddWithValue("@fname", inFname);
-                commandUserDb.Parameters.AddWithValue("@lname", inLname);
-                commandUserDb.Parameters.AddWithValue("@phn", inPhn);
-                commandUserDb.Parameters.AddWithValue("@mail", inEmail);
-                commandUserDb.Parameters.AddWithValue("@pass", inPass);
-
-                try
-                {
-                    connection.Open();
-                    commandUserDb.ExecuteNonQuery();
-                }
-                catch (SqlException sqlEx)
-                {
-                    sqlEx.Errors.ToString();
-                }
-            }
-        }
+        
     }
 }
