525ae1aa565adc50a089683a9557be47a96e7180
= <=>
2019-02-26 21:50:20 -0800

    Created APIController. Restored NuGet packages.

---
+++ WebApp/Controllers/APIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DBHelper;
+
+namespace WebApp.Controllers
+{
+    public class APIController : ApiController
+    {
+        private readonly IDbHelper _db;
+
+        public APIController(IDbHelper db) => _db = db; // DbHelper injected through constructor.
+    }
+}
