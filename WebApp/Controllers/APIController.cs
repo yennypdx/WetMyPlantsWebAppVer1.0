d32dbc26f98777971e8424aaa0c1ae4bc00dbea4
= <=>
2019-03-06 11:22:09 -0800

    Added a Login and Register method available on the API, returning status codes and JSON data.

--- WebApp/Controllers/APIController.cs
+++ WebApp/Controllers/APIController.cs
@@ -1,17 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Net;
+using System.Web.Helpers;
+using System.Web.Mvc;
 using DBHelper;
+using WebApp.Models.AccountViewModels;
 
 namespace WebApp.Controllers
 {
-    public class APIController : ApiController
+    public class ApiController : Controller
     {
         private readonly IDbHelper _db;
 
-        public APIController(IDbHelper db) => _db = db; // DbHelper injected through constructor.
+        private JsonResult BadRequest(string content) => BadRequest(Json(new {content}));
+        private JsonResult BadRequest(JsonResult content)
+        {
+            Response.Clear();
+            Response.StatusCode = (int) HttpStatusCode.BadRequest; // 400
+            return Json(content);
+        }
+
+        private JsonResult Ok(string content) => Ok(Json(new {content}));
+        private JsonResult Ok(JsonResult content)
+        {
+            Response.Clear();
+            Response.StatusCode = (int) HttpStatusCode.OK; // 200
+            return Json(content);
+        }
+
+        public ApiController(IDbHelper db) => _db = db;
+        // GET: Api
+        public string Index()
+        {
+            return "hello world!";
+        }
+
+        // POST: api/register
+        [HttpPost]
+        public ActionResult Register(RegistrationViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest("Invalid registration data");
+
+            if (!_db.CreateNewUser(model.FirstName, model.LastName, model.Phone, model.Email, model.Password))
+                return BadRequest("Unable to register user");
+
+            var loginViewModel = new LoginViewModel
+            {
+                Email = model.Email,
+                Password = model.Password,
+                RememberMe = false
+            };
+
+            return Login(loginViewModel);
+        }
+
+        // POST: api/login
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest("Invalid login data");
+
+            var token = _db.LoginAndGetToken(model.Email, model.Password);
+
+            if (token != null)
+                return Ok(token);
+
+            return BadRequest("Invalid login, check email and password and try again");
+        }
     }
 }
