ac309f8b6016c831855b08899eca27de715f4b5a
= <=>
2019-03-08 19:38:27 -0800

    Added explanatory comments to the API Controller

--- WebApp/Controllers/APIController.cs
+++ WebApp/Controllers/APIController.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
 using System.Web.Mvc;
 using DBHelper;
-using Models;
 using WebApp.Models.AccountViewModels;
 
 namespace WebApp.Controllers
@@ -13,24 +11,33 @@ namespace WebApp.Controllers
     {
         private readonly IDbHelper _db;
 
+        /* HELPER FUNCTIONS */
+
+        // Jsonify takes a string and packages it as a JSON object under the "content" key.
         private JsonResult Jsonify(string content) => Json($"{{ content: '{content}' }}");
+
+        // BadRequest takes a string or JSON object and returns it along with a 500 (BadRequest) status code
         private ActionResult BadRequest(string content) => BadRequest(Jsonify(content));
         private ActionResult BadRequest(JsonResult content) =>
             new HttpStatusCodeResult(HttpStatusCode.BadRequest, content.Data.ToString());
 
+        // Ok takes a string or JSON object and returns it along with a 200 (OK) status code
         private ActionResult Ok(string content) => Ok(Jsonify(content));
         private ActionResult Ok(JsonResult content) => 
             new HttpStatusCodeResult(HttpStatusCode.OK, content.Data.ToString());
 
+        // CTOR receives the DbHelper through Dependency Injection
         public ApiController(IDbHelper db) => _db = db;
 
         // GET: api/
+        // Test function that returns "hello world!" when you navigate to the /api URI
         public string Index()
         {
             return "hello world!";
         }
 
         // POST: api/user/register
+        // POST requests to this URI containing RegistrationViewModel data will create a new user
         [HttpPost, Route("user/register")]
         public ActionResult RegisterUser(RegistrationViewModel model)
         {
@@ -45,6 +52,8 @@ namespace WebApp.Controllers
         }
 
         // POST: api/login
+        // POST requests at this URI containing a LoginViewModel will authenticate the user
+        // and return the user's token
         [HttpPost]
         public ActionResult Login(LoginViewModel model)
         {
@@ -52,13 +61,11 @@ namespace WebApp.Controllers
 
             var token = _db.LoginAndGetToken(model.Email, model.Password);
 
-            if (token != null)
-                return Ok(token);
-
-            return BadRequest("Invalid login");
+            return token != null ? Ok(token) : BadRequest("Invalid login");
         }
 
         // DELETE: api/user/delete/id
+        // DELETE requests at this URI will delete a user from the database based on their ID
         [HttpDelete, Route("user/delete/{id}")]
         public ActionResult DeleteUser(int id)
         {
@@ -66,12 +73,13 @@ namespace WebApp.Controllers
             var user = users?.FirstOrDefault(u => u.Id.Equals(id));
             if (user == null) return BadRequest("Could not find user " + id);
 
-            var result = _db.DeleteUser(user?.Email);
+            var result = _db.DeleteUser(user.Email);
 
-            return result ? Ok("User deleted") : BadRequest("Error deleting user " + id.ToString());
+            return result ? Ok("User deleted") : BadRequest("Error deleting user " + id);
         }
 
         // GET: api/users/all
+        // INSECURE!!! GET requests at this URI will return a JSON object containing all User objects in the database
         [HttpGet, Route("users/all")]
         public JsonResult GetAllUsers()
         {
