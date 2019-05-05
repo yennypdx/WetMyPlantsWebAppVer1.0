7ac45e1e678d0469734f1d1b8b035a6eef08f9a5
= <=>
2019-05-05 16:36:27 -0700

    Notification preferences connected to AccountController, tests passing

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -101,6 +101,8 @@ namespace WebApp.Controllers
         {
             var user = (User)Session["User"];
 
+            var preferences = Db.GetNotificationPreferences(user.Id);
+
             // Convert user to ViewModel to pass to the View
             var userViewModel = new MyAccountViewModel
             {
@@ -108,7 +110,9 @@ namespace WebApp.Controllers
                 LastName = user.LastName,
                 Phone = user.Phone,
                 Email = user.Email,
-                Id = user.Id
+                Id = user.Id,
+                NotifyEmail = preferences["Email"],
+                NotifyPhone = preferences["Phone"]
             };
 
             return View(userViewModel);
@@ -177,6 +181,9 @@ namespace WebApp.Controllers
             {
                 // update the session
                 Session["User"] = user;
+
+                Db.SetEmailNotificationPreference(user.Id, model.NotifyEmail);
+                Db.SetPhoneNotificationPreference(user.Id, model.NotifyPhone);
 
                 // update the message
                 message = "Updated!";
