c2d968b353975c7325f62dfd21b0e211f6074b11
= <=>
2019-05-21 23:06:38 -0700

    Added Hubs and all related functionality.

---
+++ WebApp/Models/HubViewModels/HubViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.HubViewModels
+{
+    public class HubViewModel
+    {
+        public List<Hub> Hubs { get; set; } = new List<Hub>();
+    }
+}
