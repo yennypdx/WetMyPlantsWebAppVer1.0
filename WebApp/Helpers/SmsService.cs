1dbe7964d4ef649a1fd43144072a4b35dbb3c959
= <=>
2019-05-27 20:19:13 -0700

    Cleaned and optimized the SmsService class.

--- WebApp/Helpers/SmsService.cs
+++ WebApp/Helpers/SmsService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Twilio;
+﻿using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
 namespace WebApp.Helpers
@@ -13,16 +9,13 @@ namespace WebApp.Helpers
         {
             var accountId = Constants.TwilioAccountId;
             var authenticationToken = Constants.TwilioAuthenticationToken;
-            //const string authToken = "cab21f1579fd511e71c56bc45fcc2dbc";
-            string completeNumber = "+1" + destinationNumber;
 
             TwilioClient.Init(accountId, authenticationToken);
 
             var smsMessage = MessageResource.Create(
             body: message,
-            //from: new Twilio.Types.PhoneNumber("+19713184244"),
             from: new Twilio.Types.PhoneNumber(Constants.TwilioPhoneNumber),
-            to: new Twilio.Types.PhoneNumber(completeNumber)
+            to: new Twilio.Types.PhoneNumber($"+1{destinationNumber}")
             );
         }
     }
