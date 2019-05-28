11200624caef09a1b0c1ca35570de3f55d1efb9a
= <=>
2019-05-27 20:18:13 -0700

    Reformatted the Constants class.

--- WebApp/Helpers/Constants.cs
+++ WebApp/Helpers/Constants.cs
@@ -4,34 +4,37 @@
     {
         public static string TwilioAccountId
         {
-            get
-            {
-                return "AC3dfa39c6c58dba42c4867c99fb626324";
-            }
+            get => "AC3dfa39c6c58dba42c4867c99fb626324";
         }
 
         public static string TwilioAuthenticationToken
         {
-            get
-            {
-                return "cab21f1579fd511e71c56bc45fcc2dbc";
-            }
+            get => "cab21f1579fd511e71c56bc45fcc2dbc";
         }
 
         public static string TwilioPhoneNumber
         {
-            get
-            {
-                return "+19713184244";
-            }
+            get => "+19713184244";
         }
 
         public static string SendGridApiKey
         {
-            get
-            {
-                return "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";
-            }
+            get => "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";
+        }
+
+        public static string ResetPasswordEmail
+        {
+            get => "resetpassword@wetmyplants.com";
+        }
+
+        public static string WetMyPlantsTeamName
+        {
+            get => "WetMyPlants Team";
+        }
+
+        public static string DoNotReply
+        {
+            get => "donotreply@wetmyplants.com";
         }
     }
 }
